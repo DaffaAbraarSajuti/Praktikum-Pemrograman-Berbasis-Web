bcf10fc61ed98a01ba1383625bf5c7c321863d25
DaffaAbraarSajuti <148104478+DaffaAbraarSajuti@users.noreply.github.com>
2024-04-19 16:45:21 +0700

    mengupload praktikum 3

---
+++ Praktikum03_PBW/main.go
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+package main
+
+import (
+	"fmt"
+	"Praktikum03_PBW/ratarata"
+)
+
+func main() {
+	nilaiSiswa := []int {80, 75, 90, 85, 60}
+
+	rataRata := ratarata.HitungRataRata(nilaiSiswa)
+
+	fmt.Println("Rata-rata nilai siswa", rataRata)
+=======
+package main
+
+import (
+	"fmt"
+	"Praktikum03_PBW/ratarata"
+)
+
+func main() {
+	nilaiSiswa := []int {80, 75, 90, 85, 60}
+
+	rataRata := ratarata.HitungRataRata(nilaiSiswa)
+
+	fmt.Println("Rata-rata nilai siswa", rataRata)
+>>>>>>> e375182dfa6a9d4c0f01e3182adb14aa67940038
+}
---
+++ Praktikum03_PBW/ratarata/ratarata.go
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+package ratarata
+
+func HitungRataRata(nilaiSiswa []int) float64 {
+	total := 0
+	for _, nilai := range nilaiSiswa {
+		total += nilai 
+	}
+
+	rataRata := float64(total) / float64(len(nilaiSiswa))
+	return rataRata
+=======
+package ratarata
+
+func HitungRataRata(nilaiSiswa []int) float64 {
+	total := 0
+	for _, nilai := range nilaiSiswa {
+		total += nilai 
+	}
+
+	rataRata := float64(total) / float64(len(nilaiSiswa))
+	return rataRata
+>>>>>>> e375182dfa6a9d4c0f01e3182adb14aa67940038
+}
